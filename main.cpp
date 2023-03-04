c0700c2dcc0ae2fdc69aa40fdf598914d9ade86d
kostasp31 <kostaspetrakis2003@gmail.com>
2023-03-04 14:39:05 +0200

    known_command

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 #include <string>
 #include <set>
+#include <cstdlib>
 
 using namespace std;
+
+class board {
+    char** table = NULL;
+
+};
+
+class game {
+    //board brd = new board();
+
+};
 
 void setCommands(set<string>& st) {
     st.insert("name");
@@ -20,12 +31,27 @@ void setCommands(set<string>& st) {
     st.insert("showboard");
 }
 
+void printCommands(const set<string>& commands) {
+    for (string const& str : commands)
+        std::cout << str << '\n';
+}
+
+void exists(const set<string>& commands, const string& com) {
+    const bool inSet = commands.find(com) != commands.end();
+    if (inSet)
+        cout << "= true\n\n";
+    else
+        cout << "= false\n\n";
+}
+
 int main(void) {
+    string comm;
     set<string> commands;
     setCommands(commands);
 
-    for (string const& str : commands) {
-        std::cout << str << ' ';
-    }
+    cin >> comm;
+    exists(commands, comm);
+
+
     return 0;
 }
