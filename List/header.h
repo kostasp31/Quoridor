bfcbd068ecc4d829cf7994082d5d2b3ceb84cc2f
kostasp31 <kostaspetrakis2003@gmail.com>
2023-03-25 00:09:34 +0200

    Tidy up

---
+++ List/header.h
@@ -0,0 +1,39 @@
+#include <string>
+
+struct Node {
+    std::string value;
+    Node* next;
+    Node() {
+        this->value = "";
+        this->next = NULL;
+    }
+    Node(std::string& str, Node* nxt) {
+        //strcpy(value, str);
+        this->value = str;
+        this->next = nxt;
+    }
+};
+
+struct List {
+    Node* first = NULL;
+    /*List() {
+        first = NULL;
+    }*/
+    List(std::string& element) {
+        first = new Node(element, NULL);
+    }
+    ~List(void) {
+        Node* temp = first;
+        while (temp != NULL) {
+            Node* tmp = temp;
+            delete temp;
+            temp = tmp->next;
+        }
+    }
+};
+
+void printList(List&);
+void insertList(List&, std::string);
+void deleteFirst(List&);
+void deleteLast(List&);
+int numberOfItems(List&);
